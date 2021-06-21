2e526e24e6038f11c926ce0b48fce0173eaa8a09
Lariod <nguyenhuythinh7@gmail.com>
2021-06-21 19:37:33 +0700

    quit button : done again

--- Assets/CanvasManager.cs
+++ Assets/CanvasManager.cs
@@ -1,13 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
-#if UNITY_EDITOR //check if running in unity editor
-using UnityEditor;
-#endif 
 using UnityEngine;
 
 public class CanvasManager : MonoBehaviour
 {
-
     // Start is called before the first frame update
     void Start()
     {
@@ -15,17 +11,8 @@ public class CanvasManager : MonoBehaviour
     }
 
     // Update is called once per frame
-    private void Update()
+    void Update()
     {
         
-    }
-
-    public void ExitGame()
-    {
-    #if UNITY_EDITOR
-        EditorApplication.ExitPlaymode();
-    #else
-    Application.Quit();
-    #endif
     }
 }
